3063991eef8376d53c2799c3ff6d79e52c046860
Nguyenbac <nguyenbac2639@gmail.com>
2020-05-15 15:38:20 +0700

    commit

--- Bai_Tap 2020-03-30/code/rec.cpp
+++ Bai_Tap 2020-03-30/code/rec.cpp
@@ -1,13 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define int long long
+
 using namespace std;
 
-int main() {
-  freopen("rec.inp", "r", stdin);
-  freopen("rec.out", "w", stdout);
+int n,m,p;
+int q[1000000];
+int32_t main()
+{
+    freopen("REC.INP","r",stdin);
+    freopen("REC.OUT","w",stdout);
+    scanf("%lld%lld%lld", &n, &m, &p);
+    int ans=0;
+    if (p%2!=0) p++;
+    p/=2;
+    for (int i=1;i<=999999;i++) q[i]=q[i-1]+i;
+    int x,y,u,v;
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
 
-  int n, m, p;
-  cin >> n >> m >> p;
-  cout << n*(n+1)/2*m*(m+1)/2;
+            if (i+p-1>n) {
+                x=n;
+                y=j+p-(n-i);
+            } else {
+                x=i+p-1;
+                y=j+1;
+            }
+            if (x>n || y>m) break;
+           // if (y>m || (y-j)+(x-i)<p) break;
+            if (j+p-1>m) {
+                v=m;
+                u=i+p-(m-j);
 
-  return 0;
+            } else {
+                u=i+1;
+                v=j+p-1;
+            }
+            if (u>n || v>m) break;
+            //if (u>n || (u-i)+(v-j)<p) break;
+            //int r=0;
+          //  if (n-x-1>0) r=q[n-x-1];
+            ans+=(q[n-u+1]-q[n-x]+(m-v)*(n-u+1));
+
+        }
+    }
+    cout<<ans;
+    return 0;
 }
