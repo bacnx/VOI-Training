2fd6c228b220f353c66a5f4b35ced6558ef23711
Nguyenbac <nguyenbac2639@gmail.com>
2020-05-27 16:42:00 +0700

    commit

---
+++ Bai_Tap---2020-05-27---Chon-doi-tuyen/code/chocola.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxN = 100005;
+int n, m;
+// vector<int> indexType[300];
+
+int maxCount(vector<int> index) {
+  // vector<int> index = indexType[type];
+  int res = 0;
+
+  int i, j;
+  for (i = 0, j = 0; max(i, j) < (int)index.size()-1; i++) {
+    while(j < (int)index.size()-1 && (index[j+1]-index[i]) - (j+1 - i) <= m)
+      j++;
+    int t = (index[j] - index[i] + 1) + (m - (index[j] - index[i] - (j - i)));
+    res = max(res, t);
+  }
+
+  return res;
+}
+
+void solve() {
+  vector<int> indexType[300];
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    char temp; cin >> temp;
+    indexType[temp].push_back(i);
+  }
+  
+  int res = 0;
+  for (int i = 0; i < 26; i++) {
+    res = max(res, maxCount(indexType[(char)'A'+i]));
+    // indexType['A'+i].clear();
+    res = max(res, maxCount(indexType[(char)'a'+i]));
+    // indexType['A'+i].clear();
+  }
+
+  cout << res << '\n';
+}
+
+int main() {
+  freopen("chocola.inp", "r", stdin);
+  // freopen("chocola.out", "w", stdout);
+
+  int t; cin >> t;
+  while(t--)
+    solve();
+
+  return 0;
+}
