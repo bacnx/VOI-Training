643152e7a01a3ba91c5e110340a858f2570d6a6f
nxb <nguyenbac2639@gmail.com>
2020-05-31 17:45:08 +0700

    commit

---
+++ Bai_Tap---2020-05-27---Chon-doi-tuyen/code/sell_trau.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  freopen("sell.inp", "r", stdin);
+  freopen("sell.ans", "w", stdout);
+
+  int n, k; cin >> n >> k;
+  int a[300005];
+  for (int i = 1; i <= n; i++)
+    cin >> a[i];
+  
+  double res = 0;
+  for (int i = 1; i <= n; i++) {
+    int temp = 0;
+    for (int j = i; j < i+k; j++)
+      temp += a[j];
+    res = max(res, (double)temp/k);
+    for (int j = i+k; j <= n; j++) {
+      temp += a[j];
+      res = max(res, (double)temp/(j-i+1));
+    }
+  }
+
+  cout << fixed << setprecision(6) << res;
+
+  return 0;
+}
---
+++ Bai_Tap---2020-05-27---Chon-doi-tuyen/code/trinhcham.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const string NAME = "sell";
+
+long long Rand(long long l, long long r) {
+  return l + (long long)rand() * (long long)rand() % (r - l + 1);
+}
+// long long Rand(long long l, long long h)
+// {
+//     return l + ((long long)rand() * (RAND_MAX + 1) * (RAND_MAX + 1) * (RAND_MAX + 1) +
+//                 (long long)rand() * (RAND_MAX + 1) * (RAND_MAX + 1) +
+//                 (long long)rand() * (RAND_MAX + 1) +
+//                 rand()) % (h - l + 1);
+// }
+
+int main() {
+  srand(time(NULL));
+
+  system(("g++ " + NAME + ".cpp -o " + NAME).c_str());
+  system(("g++ " + NAME + "_trau.cpp -o" + NAME + "_trau").c_str());
+
+  for (int iTest = 1; iTest <= 100; iTest++) {
+    ofstream inp((NAME + ".inp").c_str());
+      int n = Rand(1, 10), k = (Rand(1, n));
+      inp << n << " " << k << '\n';
+      for (int i = 1; i <= n; i++)
+        inp << Rand(1, 10) << ' ';
+    inp.close();
+
+    system(("./" + NAME).c_str());
+    system(("./" + NAME + "_trau").c_str());
+    
+    if (system(("diff " + NAME + ".out " + NAME + ".ans").c_str()) != 0) {
+      cout << "Test " << iTest << ": WRONG!\n";
+      return 0;
+    } else {
+      cout << "Test " << iTest << ": CORRECT!\n";
+    }
+  }
+
+  return 0;
+}
