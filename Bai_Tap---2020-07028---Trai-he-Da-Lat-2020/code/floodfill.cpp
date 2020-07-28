d21c36edcb2608247f9d7a14123fb5de4cab369f
Bac Nguyen <nxbac@pop-os.localdomain>
2020-07-28 11:21:50 +0700

    commit

---
+++ Bai_Tap---2020-07028---Trai-he-Da-Lat-2020/code/floodfill.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, res = 0;
+stack<int> sta;
+vector<bool> isSta(5001, false);
+
+void Pop(int x) {
+  while (sta.top() != x) {
+    isSta[sta.top()] = false;
+    sta.pop();
+    res++;
+  }
+}
+
+int main() {
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    int c; cin >> c;
+    
+    if (!isSta[c]) {
+      sta.push(c);
+      isSta[c] = true;
+      continue;
+    }
+    if (c != sta.top()) {
+      if (isSta[c])
+        Pop(c);
+      else {
+        sta.push(c);
+        isSta[c] = true;
+      }
+    }
+  }
+  
+  res += (int)sta.size() - 1;
+  cout << res;
+
+  return 0;
+}
---
+++ Bai_Tap---2020-07028---Trai-he-Da-Lat-2020/code/flycar.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k, a[401];
+int d[401];
+bool fre[401];
+
+int main() {
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    d[i] = a[i];
+    fre[i] = true;
+  }
+
+  int nowK = n-1;
+  while (nowK > k) {
+    int x, minD = INT_MAX;
+    for (int i = 1; i < n; i++) {
+      if (fre[i] && abs(d[i+1] - d[i]) < minD) {
+        minD = abs(d[i+1] - d[i]);
+        x = i;
+      }
+    }
+    fre[x] = false;
+    d[x] = d[x+1] = max(d[x], d[x+1]);
+    nowK--;
+  }
+
+  long long res = 0;
+  for (int i = 1; i <= n; i++)
+    res += (long long)(d[i] - a[i]);
+  
+  cout << res;
+
+  return 0;
+}
