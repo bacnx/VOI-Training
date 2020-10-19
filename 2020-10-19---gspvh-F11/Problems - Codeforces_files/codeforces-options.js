a22afb593eb0bb872c3aab696a9f82fe4b978173
Bac Nguyen <nxbac@pop-os.localdomain>
2020-10-19 16:59:24 +0700

    commit

---
+++ 2020-10-19---gspvh-F11/Problems - Codeforces_files/codeforces-options.js
@@ -0,0 +1,6 @@
+window.codeforcesOptions = [];
+window.codeforcesOptions.subscribeServerUrl = "gspvh.contest.codeforces.com";
+window.codeforcesOptions.standaloneGroupDomain = "contest.codeforces.com";
+window.codeforcesOptions.timeZoneNoticeCaption = "in timezone UTC";
+window.codeforcesOptions.timeZoneTitleCaption = "timezone offset";
+window.codeforcesOptions.notPublicCaption = "not publicly available";
---
+++ 2020-10-19---gspvh-F11/code/couples.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<int, int>> aboveA, belowA, aboveB, belowB;
+vector<pair<int, int>> ans;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    freopen("couples.inp", "r", stdin);
+    freopen("couples.out", "w", stdout);
+
+    int t; cin >> t;
+    int m; cin >> m;
+    for (int i = 1; i <= m; i++) {
+        int a; cin >> a;
+        if (a < 0) belowA.push_back(make_pair(-a, i));
+        else aboveA.push_back(make_pair(a, i));
+    }
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        int a; cin >> a;
+        if (a < 0) belowB.push_back(make_pair(-a, i));
+        else aboveB.push_back(make_pair(a, i));
+    }
+
+    sort(aboveA.begin(), aboveA.end());
+    sort(belowA.begin(), belowA.end());
+    sort(aboveB.begin(), aboveB.end());
+    sort(belowB.begin(), belowB.end());
+
+    int j = 0;
+    for (int i = 0; i < (int)belowA.size(); i++) {
+        if (belowA[i].first > aboveB[j].first) {
+            ans.push_back(make_pair(belowA[i].second, aboveB[j].second));
+            j++;
+        }
+        if (j == (int)aboveB.size()) break;
+    }
+    j = 0;
+    for (int i = 0; i < (int)belowB.size(); i++) {
+        if (belowB[i].first > aboveA[j].first) {
+            ans.push_back(make_pair(aboveA[j].second, belowB[i].second));
+            j++;
+        }
+        if (j == (int)aboveA.size()) break;
+    }
+
+    cout << (int)ans.size() << '\n';
+    for (auto i : ans)
+        cout << i.first << ' ' << i.second << '\n';
+
+    return 0;
+}
