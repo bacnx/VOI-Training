faf6d8f8ddb2851c871d7a97ee9d84c5c20b1351
Bac Nguyen <nxbac@pop-os.localdomain>
2020-11-03 15:06:02 +0700

    solve square (AC)

--- 2020-10-31---WC2020-DN-QNam-QNgai-DLK/Code/square.cpp
+++ 2020-10-31---WC2020-DN-QNam-QNgai-DLK/Code/square.cpp
@@ -1,16 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define int long long
+
+int32_t main() {
     freopen("square.inp", "r", stdin);
     freopen("square.out", "w", stdout);
 
-    long long k; cin >> k;
+    int k; cin >> k;
+    if (k == 0) { cout << '0'; return 0; }
 
-    if (k == 0) cout << '0';
-    else if ((k & 1) == 0) cout << "none";
-    else if (k > 0) cout << (k-1)/2 + 1;
-    // else
+    int ans = LLONG_MAX;
+    for (int i = -(int)sqrt(abs(k)); i <= (int)sqrt(abs(k)); i++) if (i && k % i == 0) {
+        int a = i, b = k/i;
+        if ((a + b) % 2 != 0) continue;
+        int y = (a + b) / 2, x = max(a, b) - y;
+        if (min(x, y) >= 0)
+            ans = min(ans, y);
+    }
+    
+    if (ans == LLONG_MAX) cout << "none";
+    else cout << ans;
 
     return 0;
 }
