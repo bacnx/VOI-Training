e491e16f9c71af83c3a2f58768f142a6a4b95611
NguyenBac <nguyenbac2639@gmail.com>
2021-03-14 10:17:16 +0700

    Solve: mansion (??/100)

--- 2021-03-02---Bai-tap-do-thi-thay-Dong/mansion.cpp
+++ 2021-03-02---Bai-tap-do-thi-thay-Dong/mansion.cpp
@@ -5,15 +5,20 @@ typedef pair<int, int> pii;
 #define fi first
 #define se second
 
-const int maxN = 2e5 + 5;
+#define maxN 200005
 
 int n, m, k;
 vector<pii> rooms;
 vector<pii> adj[maxN][2];
 
+vector<pii> row[maxN], col[maxN];
+
+int cntState;
+pii state[maxN * 2];
+int id[maxN][2];
+int d[maxN * 2];
 
 void initGraph() {
-	vector<pii> row[maxN], col[maxN];
 	for (int i = 0; i < (int)rooms.size(); i++) {
 		int x = rooms[i].fi, y = rooms[i].se;
 		row[x].push_back(pii(y, i));
@@ -46,16 +51,54 @@ void initGraph() {
 void printGraph() {
 	for (int i = 0; i < k + 2; i++) {
 		cout << i << '\n';
-		for (pii e : adj[i][0]) {
+		for (pii e : adj[i][1]) {
 			cout << e.fi << ' ' << e.se << '\n';
 		}
 	}
 }
 
+void initState() {
+	cntState = 0;
+	for (int i = 0; i < k + 2; i++) for (int j = 0; j < 2; j++) {
+		state[cntState] = pii(i, j);
+		id[i][j] = cntState;
+		cntState++;
+	}
+}
+
+void dijkstra(int u0, int mask) {
+	memset(d, -1, sizeof d);
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	d[id[u0][mask]] = 0;
+	pq.push(pii(d[id[u0][mask]], id[u0][mask]));
+
+	while (!pq.empty()) {
+		int cur = pq.top().se, wCur = pq.top().fi; pq.pop();
+		if (wCur != d[cur]) continue;
+		int u = state[cur].fi, mask = state[cur].se;
+
+
+		for (pii e : adj[u][mask]) {
+			int v = e.fi, w = e.se, newState = id[v][mask];
+			if (d[newState] == -1 || d[newState] > d[cur] + w) {
+				d[newState] = d[cur] + w;
+				pq.push(pii(d[newState], newState));
+			}
+		}
+		for (pii e : adj[u][mask ^ 1]) {
+			int v = e.fi, w = e.se + 1, newState = id[v][mask ^ 1];
+			if (d[newState] == -1 || d[newState] > d[cur] + w) {
+				d[newState] = d[cur] + w;
+				pq.push(pii(d[newState], newState));
+			}
+		}
+	}
+} 
+
 int main() {
 	freopen("input.txt", "r", stdin);
 
-	cin >> n >> m >> k;
+	cin >> m >> n >> k;
 	rooms.push_back(pii(1, 1));
 	rooms.push_back(pii(m, n));
 	for (int i = 0; i < k; i++) {
@@ -64,8 +107,17 @@ int main() {
 	}
 
 	initGraph();
+	initState();
+	dijkstra(0, 0);
 
-	printGraph();
+	int res = 1e9; {
+		if (d[id[1][0]] != -1 && d[id[1][0]] < res)
+			res = d[id[1][0]];
+		if (d[id[1][1]] != -1 && d[id[1][1]] < res)
+			res = d[id[1][1]];
+		if (res == 1e9) res = -1;
+	}
+	cout << res;
 
 	return 0;
 }
